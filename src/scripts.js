7199b08f7110db15a0d5f8f3043bab896a17086f
David Kwon <dkwon1223@gmail.com>
2024-02-29 22:38:35 -0700

    Change import to styles.scss and remove styles.css

--- src/scripts.js
+++ src/scripts.js
@@ -3,7 +3,7 @@
 
 // An example of how you tell webpack to use a CSS (SCSS) file
 import { fetchTravelersData } from './apiCalls';
-import './css/styles.css';
+import './css/styles.scss';
 
 // An example of how you tell webpack to use an image (also need to link to it in the index.html)
 import './images/turing-logo.png'
