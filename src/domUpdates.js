cbb83c33b42dc9cfeae6593345fbaa89977d9494
David Kwon <dkwon1223@gmail.com>
2024-03-03 18:28:52 -0700

    Add basic inputs for trip request widget

--- src/domUpdates.js
+++ src/domUpdates.js
@@ -36,3 +36,10 @@ signOutButton.addEventListener("click", () => {
     location.reload();
 })
 
+const picker = new easepick.create({
+    element: document.getElementById('datepicker'),
+    css: [
+      'https://cdn.jsdelivr.net/npm/@easepick/bundle@1.2.1/dist/index.css',
+    ],
+    plugins: ['RangePlugin']
+});
