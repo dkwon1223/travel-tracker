e39023a87937b1b2061d7efa67c1ac8ff6336204
David Kwon <dkwon1223@gmail.com>
2024-03-04 16:43:41 -0700

    Add basic structure in HTML and CSS for trips widget

--- src/domUpdates.js
+++ src/domUpdates.js
@@ -9,7 +9,6 @@ const logInButton = document.querySelector("#logInButton");
 const usernameInput = document.querySelector("#usernameInput");
 const passwordInput = document.querySelector("#passwordInput");
 const loginErrorMessage = document.querySelector(".login-error-message");
-
 const travelerDashboard = document.querySelector(".traveler-dashboard");
 const userHeader = document.querySelector(".nav-user-info");
 const signOutButton = document.querySelector("#signOutButton");
